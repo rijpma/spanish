b00f5c392aae4a8e449477a4b5835891d36a088d
rijpma <auke.rijpma@gmail.com>
2021-07-21 12:32:32 +0200

    update maps

--- src/maps.R
+++ src/maps.R
@@ -9,89 +9,88 @@ source("fun.R")
 deaths = data.table::fread("../dat/deaths.csv", na.strings = "")
 municipalities = data.table::fread("../dat/spatialagg.txt")
 coverage = fread("../dat/HDNG_OpenArch.txt")
-
-deaths = municipalities[, list(amco = ACODE, corop = Corop, egg = EGG)][deaths, on = "amco"]
-
 nl = read_sf("../dat/nl_1918/nl_1918.shp")
 
-# death certificate coverage v1
-toplot = merge(
-    nl,
-    coverage[, list(coverage = 1 - mean(diffperc / 100)), by = list(acode = ACODE)],
-    by = "acode",
-    all.x = TRUE)
+deaths = merge(
+    x = deaths,
+    y = municipalities[, list(amco = ACODE, corop = Corop, egg = EGG)],
+    by = "amco",
+    all.x = TRUE, all.y = FALSE)
 
-pdf("../out/certificate_coverage.pdf", width = 6)
-plot(toplot[, "coverage"], pal = viridisLite::viridis)
-dev.off()
+# death certificate coverage
+meancov = coverage[value > 0, list(diffperc = mean(diffperc), value = mean(value), openach = mean(n_death_OpenArch)), by = list(acode = ACODE)]
+meancov[, include := ifelse(abs(diffperc) >= 0 & abs(diffperc) < 50, TRUE, FALSE)]
+municipalities[ACODE %in% meancov[include == FALSE, acode], .N, by = Provincie]
 
-# death certificate coverage v2 to get rid of yellow Buren and 1/0 scale
 toplot = merge(
   nl,
-  coverage[, list(coverage = 1 ), by = list(acode = ACODE)],
+  coverage[, list(coverage = TRUE), by = list(acode = ACODE)],
   by = "acode",
   all.x = TRUE)
 
-toplot[toplot$acode == 10980, c("coverage")] <- 0
+toplot$coverage[is.na(toplot$coverage)] <- FALSE
 
 pdf("../out/certificate_coverage.pdf", width = 6)
-plot(toplot[, "coverage"], pal = viridisLite::viridis)
+plot(toplot[, "coverage"], pal = viridisLite::viridis, main = "Certificate coverage")
 dev.off()
 
-# hisco_2 map to get only usable occs for pr_age >= 15
-deaths[HISCO == "99999" | grepl("-", HISCO), HISCO := NA,]
-
 # coverage other variables
+# hisco_2 map to get only usable occs for pr_age >= 15
+deaths[HISCO == "99999" | grepl("-", HISCO), HISCO := NA]
+
 toplot = merge(
     nl,
     deaths[, 
         list(hisco = mean(!is.na(HISCO[pr_age >= 15 & pr_age <= 80]), na.rm = TRUE),
              age = mean(!is.na(pr_age), na.rm = TRUE),
              sex = mean(!is.na(pr_gender), na.rm = TRUE),
-             date = mean(!is.na(death_date), na.rm = TRUE)), 
+             date = mean(!is.na(death_date), na.rm = TRUE),
+             N = .N), 
          by = list(acode = amco)],
     by = "acode",
     all.x = TRUE)
 
 # fix scales by setting smallest munic
 toplot[toplot$acode == 10980, c("hisco", "age", "sex", "date")] <- 0
+toplot$hisco[toplot$hisco >= 0.6 & toplot$N < 10] <- NA
+toplot[order(-toplot$hisco), ]
 
 pdf("../out/age_coverage.pdf", width = 6)
-plot(toplot[, "age"], pal = viridisLite::viridis)
+plot(toplot[, "age"], pal = viridisLite::viridis, main = "Age coverage")
 dev.off()
 pdf("../out/sex_coverage.pdf", width = 6)
-plot(toplot[, "sex"], pal = viridisLite::viridis)
+plot(toplot[, "sex"], pal = viridisLite::viridis, main = "Sex coverage")
 dev.off()
 pdf("../out/date_coverage.pdf", width = 6)
-plot(toplot[, "date"], pal = viridisLite::viridis)
+plot(toplot[, "date"], pal = viridisLite::viridis, main = "Date coverage")
 dev.off()
 pdf("../out/occupations_coverage.pdf", width = 6)
-plot(toplot[, "hisco"], pal = viridisLite::viridis)
+plot(toplot[, "hisco"], pal = viridisLite::viridis, main = "Occupation coverage")
 dev.off()
+
 # occupations range 0-0.4 because age < 20 age > 60 and f age > 40 rarely have one
-
-# hisco_2 map to get only usable occs for pr_age >= 15
-
-plot(ecdf(toplot$hisco))
-as.data.table(toplot)[, .N, by = round(hisco, 1)][order(round)]
-
-# age should be bigger than say 0.2
 # hisco should be bigger than say 0.2 for the relevant agegroup (0.1 overall)
+# age should be bigger than say 0.2
 # sex and date should be present
 # and it should be in the coverage list
 
 # emr plot
-deaths[, agegroup := pr_age]
-deaths[data.table::between(pr_age, 10, 70), agegroup := 40]
-toplot = excess(deaths[sep_dec == TRUE], aggvrbs = c("year", "amco", "agegroup"))
+toplot = excess(deaths[sep_dec == TRUE & data.table::between(pr_age, 12, 79)], 
+    aggvrbs = c("year", "amco"))
 
+# note: subsetting on age and sep-dec changes the emr picture somewhat compared to the HLC paper
 toplot = merge(
     nl,
     toplot,
     by.x = "acode", by.y = "amco",
     all.x = TRUE)
+
+# trim outliers
+toplot$emr2 <- cut(toplot$emr, c(0, 1, 2, 3, 4, 5, 55))
+levels(toplot$emr2)[6] <- ">5"
+
 pdf("../out/excessmap.pdf", width = 6)
-plot(toplot[, "emr"], pal = viridisLite::viridis)
+plot(toplot[, "emr2"],  pal = viridisLite::viridis, main = "Excess mortality rate")
 dev.off()
 
 # corop and egg regions
@@ -108,5 +107,4 @@ par(mfrow = c(1, 2))
 plot(eggs[, "Group.1"], col = NA, main = "EGG", reset = FALSE)
 plot(corops[, "Group.1"], col = NA, main = "Corop", reset = FALSE)
 dev.off()
-# add hdng population 
-# plot excess for all nld?
+# add hdng population
